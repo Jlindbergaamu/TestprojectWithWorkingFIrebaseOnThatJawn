2111f10d1683e02b71b7a89c12ffba403d29b426
Justin Lindberg <jlindber@bulldogs.aamu.edu>
2019-10-28 16:06:55 -0500

    Initial Commit

---
+++ Testproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testproject
+//
+//  Created by Justin Lindberg on 10/28/19.
+//  Copyright © 2019 Justin Lindberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
